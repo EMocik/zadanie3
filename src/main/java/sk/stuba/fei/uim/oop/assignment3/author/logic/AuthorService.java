bbf1abb65dd679c55ca3560c320415a6370c032a
Erik Môcik <erik.mocik@gmail.com>
2022-05-12 00:52:37 +0200

    done

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/author/logic/AuthorService.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/author/logic/AuthorService.java
@@ -11,7 +11,7 @@ import sk.stuba.fei.uim.oop.assignment3.book.data.IBookRepository;
 import sk.stuba.fei.uim.oop.assignment3.exceptions.NotFoundException;
 
 import java.util.List;
-//test for push
+
 @Service
 public class AuthorService implements IAuthorService {
 
